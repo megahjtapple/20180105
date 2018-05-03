bf78c50893347dd5a2e49b3ae90eb74b118df63d
user000 <user000@localhost>
2018-05-02 22:03:38 -0700

    asdf

--- AIProject/pkg2/2018-05-01-Tensorflow6Session.py
+++ AIProject/pkg2/2018-05-01-Tensorflow6Session.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+# Youtube video: https://www.youtube.com/watch?v=HhjtJ73AwIY&list=PLXO45tsB95cKI5AIlf5TxxFPzb-0zeVZ8&index=9
 
 import tensorflow as tf
 
@@ -9,9 +10,42 @@ matrix2 = tf.constant([[2],
 product = tf.matmul(matrix1, matrix2) #matrix multiply np.dor(m1, m2)
 
 #method1
-sess = tf.Session()
-result = sess.run(product)
-print(result)
-sess.close()
+#sess = tf.Session()
+#result = sess.run(product)
+#print(result)
+#sess.close()
+
+#method2
+with tf.Session() as sess:
+    result = sess.run(product)
+    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
